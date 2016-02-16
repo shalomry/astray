5e3f96dd2e79e1499654590b32d561e49c291d48
Katherine Bernstein <katherinebernstein@Katherines-MacBook-Pro.local>
2016-02-16 13:54:29 -0800

    add controller for story creation, connect fields/buttons to controller, have map automatically center on user's location

---
+++ app/Astray/Astray/CreateStoryController.swift
@@ -0,0 +1,81 @@
+//
+//  CreateStoryController.swift
+//  Astray
+//
+//  Created by Katherine Bernstein on 2/16/16.
+//  Copyright © 2016 yes. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import Firebase
+import GeoFire
+
+class CreateStoryController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var storyTitle: UITextField!
+    @IBOutlet weak var storyDescription: UITextField!
+    @IBOutlet weak var createStoryButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        if let currUid = appDelegate.currUid {
+            
+            if (CLLocationManager.locationServicesEnabled()) {
+                locationManager = CLLocationManager()
+                locationManager.delegate = self
+                locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                locationManager.requestAlwaysAuthorization()
+                locationManager.startUpdatingLocation()
+                
+                let userTrackingArrow = MKUserTrackingBarButtonItem(mapView: self.mapView)
+                self.toolbarItems = [userTrackingArrow]
+                self.navigationController?.setToolbarHidden(false, animated: false)
+                mapView.showsUserLocation = true
+                mapView.delegate = self
+                self.mapView.setUserTrackingMode(MKUserTrackingMode.Follow, animated: false);
+
+                let myRootRef = Firebase(url:"https://astray194.firebaseio.com")
+                let geoFire = GeoFire(firebaseRef: myRootRef)
+            }
+        } else {
+            self.navigateToView("LoginView")
+        }
+    }
+    
+    func navigateToView(view:String) {
+        if let nextView = self.storyboard?.instantiateViewControllerWithIdentifier(view) {
+            self.navigationController?.pushViewController(nextView, animated: true)
+        }
+    }
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = manager.location {
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            self.mapView.setRegion(region, animated: true)
+            
+            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            if let currUid = appDelegate.currUid {
+                let myRootRef = Firebase(url:"https://astray194.firebaseio.com")
+                let geoFire = GeoFire(firebaseRef: myRootRef)
+                geoFire.setLocation(location, forKey: currUid)
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
