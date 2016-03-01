fb5ea5fd6c4dd1140f2a2f29009de725d5485610
Katherine Bernstein <katherinebernstein@rescomp-15-279207.stanford.edu>
2016-02-29 17:54:23 -0800

    -change email in login/auth also when you change in settings -require password to change email -show appropriate error messages when it can't change password -remove stumble mode button

--- app/Astray/Astray/UserController.swift
+++ app/Astray/Astray/UserController.swift
@@ -25,11 +25,20 @@ class UserController : UIViewController, UIActionSheetDelegate {
     @IBOutlet weak var profileEmailLabel: UILabel!
     @IBOutlet weak var newEmailField: UITextField!
     @IBOutlet weak var goToStoriesButton: UIButton!
+    @IBOutlet weak var passwordConfirmation: UITextField!
+    @IBOutlet weak var settingsError: UILabel!
+    
+    let invalidPasswordText = "The password you entered was incorrect."
+    let invalidEmailText = "Please enter a valid email address."
+    let emailTakenText = "An account with that email already exists."
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Firebase(url:"https://astray194.firebaseio.com/Users")
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        if self.settingsError != nil {
+            self.settingsError.text = ""
+        }
         if let currUid = appDelegate.currUid {
             let currUserRef = ref.childByAppendingPath(currUid)
             currUserRef.observeEventType(.Value, withBlock: { snapshot in
@@ -78,22 +87,51 @@ class UserController : UIViewController, UIActionSheetDelegate {
         }
     }
     
-    @IBAction func updateProfile() {
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        if let currUid = appDelegate.currUid {
-            let currUserRef = ref.childByAppendingPath(currUid)
-            let bioRef = currUserRef.childByAppendingPath("bio")
-            bioRef.setValue(newBioField.text)
-            let emailRef = currUserRef.childByAppendingPath("email")
-            emailRef.setValue(newEmailField.text)
-        }
-        self.navigateToView("ProfileView")
-    }
-    
     @IBAction func logout() {
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.currUid = nil
         self.navigateToView("LoginView")
     }
     
+    @IBAction func updateProfile() {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        if let currUid = appDelegate.currUid {
+            let currUserRef = ref.childByAppendingPath(currUid)
+            if let currUid = appDelegate.currUid {
+                let currUserRef = ref.childByAppendingPath(currUid)
+                currUserRef.observeEventType(.Value, withBlock: { snapshot in
+                    if let email = snapshot.value.objectForKey("email") {
+                        if self.newEmailField.text != "\(email)" {
+                            print("CHANGING EMAIL")
+                            let ref = Firebase(url:"https://astray194.firebaseio.com")
+                            ref.changeEmailForUser("\(email)", password: self.passwordConfirmation.text, toNewEmail: self.newEmailField.text, withCompletionBlock: { error in
+                                if error != nil {
+                                    print(error)
+                                    self.settingsError.text = "Please enter a valid email address."
+                                    if error.code == -5 {
+                                        self.settingsError.text = self.invalidEmailText
+                                    } else if error.code == -6 {
+                                        self.settingsError.text = self.invalidPasswordText
+                                    } else if error.code == -9 {
+                                        self.settingsError.text = self.emailTakenText
+                                    }
+                                } else {
+                                    print("IT WORKED")
+                                    let emailRef = currUserRef.childByAppendingPath("email")
+                                    emailRef.setValue(self.newEmailField.text)
+                                    let bioRef = currUserRef.childByAppendingPath("bio")
+                                    bioRef.setValue(self.newBioField.text)
+                                    self.navigateToView("ProfileView")
+                                }
+                            })
+                        } else {
+                            let bioRef = currUserRef.childByAppendingPath("bio")
+                            bioRef.setValue(self.newBioField.text)
+                            self.navigateToView("ProfileView")
+                        }
+                    } else { print("NOT CHANGING EMAIL") }
+                })
+            }
+        }
+    }
 }
