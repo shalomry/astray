8c4ecff0edea13a81c57dc49f049d276e9a65aea
Deger Turan <degerturan@gmail.com>
2016-03-07 01:44:38 -0800

    added voice recording

--- app/Astray/Astray/CreateStoryController.swift
+++ app/Astray/Astray/CreateStoryController.swift
@@ -11,25 +11,56 @@ import MapKit
 import CoreLocation
 import Firebase
 import GeoFire
+import AVFoundation
 
-class CreateStoryController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class CreateStoryController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, AVAudioRecorderDelegate {
     
     @IBOutlet weak var storyTitle: UITextField!
     @IBOutlet weak var storyDescription: UITextField!
     @IBOutlet weak var storyTextData: UITextField!
-    @IBOutlet weak var storyAudioData: NSData!
-    @IBOutlet weak var storyVideoData: NSData!
-    @IBOutlet weak var storyImageData: NSData!
+    @IBOutlet var storyData: String!
     @IBOutlet weak var uploadStoryButton: UIButton!
     @IBOutlet weak var mapView: MKMapView!
     var username: String?
     var userId: String?
     
+    
+    var recordButton: UIButton!
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var audioURL: NSURL!
     var locationManager: CLLocationManager!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] (allowed: Bool) -> Void in
+                dispatch_async(dispatch_get_main_queue()) {
+                    if allowed {
+                        self.loadRecordingUI()
+                        print("permission granted!")
+                    } else {
+                        // failed to record!
+//                        
+//                        audioRecorder.delegate = self
+//                        audioRecorder.meteringEnabled = true
+//                        audioRecorder.prepareToRecord()
+//                        audioRecorder.record()
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
+        
+        
         
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         if let currUid = appDelegate.currUid {
@@ -47,7 +78,7 @@ class CreateStoryController: UIViewController, MKMapViewDelegate, CLLocationMana
                 mapView.showsUserLocation = true
                 mapView.delegate = self
                 self.mapView.setUserTrackingMode(MKUserTrackingMode.Follow, animated: false);
-
+                
                 let myRootRef = Firebase(url:"https://astray194.firebaseio.com")
                 let geoFire = GeoFire(firebaseRef: myRootRef)
                 
@@ -78,6 +109,91 @@ class CreateStoryController: UIViewController, MKMapViewDelegate, CLLocationMana
         self.navigateToView("RecordingView")
     }
     
+    func finishRecording(success success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            storyData = prepareRecording()
+            recordButton.setTitle("Tap to Re-record", forState: .Normal)
+        } else {
+            recordButton.setTitle("Tap to Record", forState: .Normal)
+            print("recording failed :(")
+        }
+    }
+    
+    func prepareRecording() -> String{
+        let data = NSData(contentsOfURL: audioURL!)
+        
+        if data != "" {
+            
+            let encodeOption = NSDataBase64EncodingOptions(rawValue: 0)
+            do {
+                 try NSFileManager.defaultManager().removeItemAtURL(audioURL)
+            } catch {
+                print("OMG FILE COULDNT BE DELETED WTHH")
+            }
+            return data!.base64EncodedStringWithOptions(encodeOption)
+        }
+        return ""
+       
+    }
+    
+    func loadRecordingUI() {
+        recordButton = UIButton(frame: CGRect(x: 64, y: 64, width: 128, height: 64))
+        recordButton.setTitle("Tap to Record", forState: .Normal)
+        recordButton.titleLabel?.font = UIFont.preferredFontForTextStyle(UIFontTextStyleTitle1)
+        recordButton.addTarget(self, action: "recordTapped", forControlEvents: .TouchUpInside)
+        view.addSubview(recordButton)
+    }
+    
+    
+    func startRecording(){
+        
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let docDir = paths[0]
+        let dataPath = (docDir as NSString).stringByAppendingPathComponent("audioFile4.m4a")
+        audioURL = NSURL(fileURLWithPath: dataPath)
+        
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000.0,
+            AVNumberOfChannelsKey: 1 as NSNumber,
+            AVEncoderAudioQualityKey: AVAudioQuality.High.rawValue
+        ]
+        
+        do {
+            print(audioURL)
+            audioRecorder = try AVAudioRecorder(URL: audioURL, settings: settings)
+                        print("recorder is built0!")
+            audioRecorder.delegate = self
+                        print("recorder is built1!")
+            audioRecorder.record()
+                        print("recorder is built2!")
+
+            recordButton.setTitle("Tap to Stop", forState: .Normal)
+        } catch {
+            print("recorder couldn't be built :(")
+            finishRecording(success: false)
+        }
+    }
+    
+    func recordTapped() {
+        if audioRecorder == nil {
+            print("starting recording")
+            startRecording()
+        } else {
+            print("done with recording! kudos")
+            finishRecording(success: true)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = manager.location {
@@ -94,20 +210,6 @@ class CreateStoryController: UIViewController, MKMapViewDelegate, CLLocationMana
         }
     }
     
-    private func audioData()throws ->String {
-        
-        let sample = NSBundle.mainBundle().URLForResource("memchu", withExtension: "mp3")
-        let data = NSData(contentsOfURL: sample!)
-        
-        
-        if data != "" {
-            
-            let encodeOption = NSDataBase64EncodingOptions(rawValue: 0)
-            return data!.base64EncodedStringWithOptions(encodeOption)
-        }
-        return ""
-    }
-    
     @IBAction func uploadStoryButtonClicked() {
         let lat = mapView.centerCoordinate.latitude
         let long = mapView.centerCoordinate.longitude
@@ -119,11 +221,10 @@ class CreateStoryController: UIViewController, MKMapViewDelegate, CLLocationMana
         
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         
-        let story = UIApplication.sharedApplication().delegate as! Story
         
         do{
             let storyInfo: NSDictionary = [
-                "title": story.title,
+                "title": "tit",
                 "description": self.storyDescription.text!,
                 "author": self.username!,
                 "author_id": self.userId!,
@@ -131,44 +232,44 @@ class CreateStoryController: UIViewController, MKMapViewDelegate, CLLocationMana
                 "longitude": long,
                 "radius": rad,
                 "timestamp": time,
-                "data": try audioData(),
+                "data": storyData,
                 "fileType": "mp3"
             ]
-        
+            
             let childRef = storyRef.childByAutoId()
             childRef.setValue(storyInfo)
-        
-//            let myRootRef = Firebase(url:"https://astray194.firebaseio.com/Geo")
-//            let geoFire = GeoFire(firebaseRef: myRootRef)
-//            var query = geoFire.queryAtLocation(CLLocation(latitude: lat, longitude: long), withRadius: 0.1)
-//            var addHandle = query.observeEventType(GFEventTypeKeyEntered, withBlock: { (key: String!, location: CLLocation!) in
-//                print("Key '\(key)' entered the search area for "+(storyInfo["title"] as! String))
-//                let storiesRef = Firebase(url:"https://astray194.firebaseio.com/Users/"+key+"/availablestories")
-//                storiesRef.runTransactionBlock({
-//                    (currentData:FMutableData!) in
-//                    var storyset: Set<String> = [childRef.key as! String]
-//                    if let value: [String] = currentData.value as? [String] {
-//                        for key in value { storyset.insert(key) }
-//                    }
-//                    currentData.value = Array(storyset)
-//                    return FTransactionResult.successWithValue(currentData)
-//                })
-//            })
-//            
-//            var removeHandle = query.observeEventType(GFEventTypeKeyExited, withBlock: { (key: String!, location: CLLocation!) in
-//                print("Key '\(key)' left "+(storyInfo["title"] as! String))
-//                let storiesRef = Firebase(url:"https://astray194.firebaseio.com/Users/"+key+"/availablestories")
-//                storiesRef.runTransactionBlock({
-//                    (currentData:FMutableData!) in
-//                    var storyset: Set<String> = Set<String>()
-//                    if let value: [String] = currentData.value as? [String] {
-//                        for key in value { storyset.insert(key) }
-//                        storyset.remove(childRef.key as! String)
-//                    }
-//                    currentData.value = Array(storyset)
-//                    return FTransactionResult.successWithValue(currentData)
-//                })
-//            })
+            
+            //            let myRootRef = Firebase(url:"https://astray194.firebaseio.com/Geo")
+            //            let geoFire = GeoFire(firebaseRef: myRootRef)
+            //            var query = geoFire.queryAtLocation(CLLocation(latitude: lat, longitude: long), withRadius: 0.1)
+            //            var addHandle = query.observeEventType(GFEventTypeKeyEntered, withBlock: { (key: String!, location: CLLocation!) in
+            //                print("Key '\(key)' entered the search area for "+(storyInfo["title"] as! String))
+            //                let storiesRef = Firebase(url:"https://astray194.firebaseio.com/Users/"+key+"/availablestories")
+            //                storiesRef.runTransactionBlock({
+            //                    (currentData:FMutableData!) in
+            //                    var storyset: Set<String> = [childRef.key as! String]
+            //                    if let value: [String] = currentData.value as? [String] {
+            //                        for key in value { storyset.insert(key) }
+            //                    }
+            //                    currentData.value = Array(storyset)
+            //                    return FTransactionResult.successWithValue(currentData)
+            //                })
+            //            })
+            //
+            //            var removeHandle = query.observeEventType(GFEventTypeKeyExited, withBlock: { (key: String!, location: CLLocation!) in
+            //                print("Key '\(key)' left "+(storyInfo["title"] as! String))
+            //                let storiesRef = Firebase(url:"https://astray194.firebaseio.com/Users/"+key+"/availablestories")
+            //                storiesRef.runTransactionBlock({
+            //                    (currentData:FMutableData!) in
+            //                    var storyset: Set<String> = Set<String>()
+            //                    if let value: [String] = currentData.value as? [String] {
+            //                        for key in value { storyset.insert(key) }
+            //                        storyset.remove(childRef.key as! String)
+            //                    }
+            //                    currentData.value = Array(storyset)
+            //                    return FTransactionResult.successWithValue(currentData)
+            //                })
+            //            })
             
             let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
             print("KEY:")
