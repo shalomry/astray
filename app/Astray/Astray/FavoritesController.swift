a6c15f0e40d1b980b46e026a1cbbb5c477311395
Katherine Bernstein <katherinebernstein@DN0a2268a6.SUNet>
2016-03-06 22:35:59 -0800

    populate favorites table

--- app/Astray/Astray/FavoritesController.swift
+++ app/Astray/Astray/FavoritesController.swift
@@ -17,11 +17,17 @@ class FavoritesController : UITableViewController {
     var ref: Firebase!
     @IBOutlet var favoritesTable: UITableView!
     
+    var listOfUsernames: NSMutableArray!
+    var listOfIds: NSMutableArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        listOfUsernames = NSMutableArray()
+        listOfIds = NSMutableArray()
         ref = Firebase(url:"https://astray194.firebaseio.com/Users")
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         if let currUid = appDelegate.currUid {
+            print(currUid)
             let currUserRef = ref.childByAppendingPath(currUid)
             currUserRef.observeEventType(.Value, withBlock: { snapshot1 in
                 if let following = snapshot1.value.objectForKey("following") {
@@ -30,7 +36,8 @@ class FavoritesController : UITableViewController {
                         if id.length! > 0 {
                             self.ref.childByAppendingPath(id as! String).observeEventType(.Value, withBlock: { snapshot2 in
                                 if let username = snapshot2.value.objectForKey("username") {
-                                    //add row with username
+                                    self.listOfUsernames.addObject(username)
+                                    self.listOfIds.addObject(id as! String)
                                 }
                             })
                         }
@@ -38,5 +45,20 @@ class FavoritesController : UITableViewController {
                 }
             })
         }
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfUsernames.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+            cell.textLabel?.text = listOfUsernames[indexPath.item] as? String
+        return cell
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        favoritesTable.reloadData()
     }
 }
